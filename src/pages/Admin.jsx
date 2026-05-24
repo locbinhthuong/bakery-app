3ad7955d477ee81d640499a1a6ec713846320784
locbinhthuong <tanlocdepzai123@gmail.com>
2026-05-24 20:49:08 +0700

    fix: resolve syntax error missing div closing tag in Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -664,6 +664,7 @@ export default function Admin() {
                     ))}
                   </tbody>
                 </table>
+                </div>
               </div>
             </div>
           )}
