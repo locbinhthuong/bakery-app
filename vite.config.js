ce040748285cd2b854b83845125c8ca7b8145c3e
locbinhthuong <tanlocdepzai123@gmail.com>
2026-05-25 13:14:40 +0700

    fix: xử lý cảnh báo kích thước file và tslib để vercel build thành công

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['tslib']
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    commonjsOptions: {
+      include: [/tslib/, /node_modules/]
+    }
+  }
 })
